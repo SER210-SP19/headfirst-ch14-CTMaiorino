72fcc512b724fbf9b298fd530b4978540eaa230c
CTMaiorino <32141103+CTMaiorino@users.noreply.github.com>
2019-05-09 15:44:34 -0400

    Commit - Ch 14

---
+++ src/main/java/edu/quinnipiac/ser210/catchat/InboxFragment.java
@@ -0,0 +1,19 @@
+package edu.quinnipiac.ser210.catchat;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InboxFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState){
+        return inflator.inflate(R.layout.fragment_inbox, container, false);
+    }
+
+}
